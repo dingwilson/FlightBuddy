1e93407f6682eea1dd5f7dbf5a35ad1c1631f8df
Wilson Ding <hello@wilsonding.com>
2018-01-27 16:30:01 -0600

    Removed PeerKit and added Signal integration

--- FlightBuddy/FlightBuddy/ViewController.swift
+++ FlightBuddy/FlightBuddy/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let signal = Signal.instance
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        signal.initialize(serviceType: "signal-demo")
+        signal.delegate = self
+
+        signal.autoConnect()
+        // signal.acceptAuto()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: SignalDelegate {
+    func signal(didReceiveData data: Data, ofType type: UInt32) {
+        if type == DataType.string.rawValue {
+            let string = data.convert() as! String
+            print(string)
+        }
+    }
+
+    func signal(connectedDevicesChanged devices: [String]) {
+        if (devices.count > 0) {
+            print("Connected Devices: \(devices)")
+        } else {
+            print("No devices connected")
+        }
+    }
+
+}
---
+++ FlightBuddy/Signal/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  FlightBuddy
+//
+//  Created by Wilson Ding on 1/27/18.
+//  Copyright © 2018 Wilson Ding. All rights reserved.
+//
+
+import Foundation
+
+enum DataType: UInt32 {
+    case string = 100
+    case image = 101
+}
