2c26cb26254e603da3bc4dbcc290118782563521
Wilson Ding <hello@wilsonding.com>
2018-01-28 05:09:32 -0600

    Added UI elements for hub

--- FlightBuddy Hub/FlightBuddy Hub/ViewController.swift
+++ FlightBuddy Hub/FlightBuddy Hub/ViewController.swift
@@ -12,18 +12,11 @@ class ViewController: UIViewController {
     
     let flightService = FlightService.instance
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("View did load called...")
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
     @IBAction func didPressJoinButton(_ sender: UIButton) {
         print("Pressed button...")
         let flight: Flight = Constants.flightList[0];
         self.startConnection(flight: flight)
     }
-    
     
     private func startConnection(flight: Flight) {
         flightService.setCurrentFlight(flight: flight)
